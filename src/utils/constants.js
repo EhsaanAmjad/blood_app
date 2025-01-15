655b4df1971c0ca274690c95cd35804d65070b34
EhsaanAmjad <ehsaanmughal9@gmail.com>
2025-01-15 15:27:53 +0000

    new changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const APP_NAME = 'Blood Bank';
-// export const BASE_URL = 'https://blood-bank-backend-sz9t.onrender.com/api';
-export const BASE_URL = 'http://localhost:3001/api';
+export const BASE_URL = 'https://blood-bank-backend-sz9t.onrender.com/api';
+// export const BASE_URL = 'http://localhost:3001/api';
 export const REQUEST_HEADERS = {};
 export const DEFAULT_NO_OF_LIST_ITEMS = 6;
